7d37613da2f66e4c2a7550897197906d0987e11d
takashabe <takashabe.public@gmail.com>
2017-06-24 22:12:00 +0900

    Add forward interface

--- forward/console.go
+++ forward/console.go
@@ -1,1 +1,16 @@
 package forward
+
+import (
+	"io"
+
+	"github.com/takashabe/go-metrics/collect"
+)
+
+// MetricsWriter is write metrics interface
+type MetricsWriter interface {
+	SetSource(c *collect.Collector, metrics ...string)
+	SetDestination(w io.Writer) error
+	AddMetrics(metrics ...string) error
+	RemoveMetrics(metrics ...string) error
+	Flush() ([]byte, error)
+}

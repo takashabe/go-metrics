463ad184bc4c637ff52dd1a80c254854f9d4ce79
takashabe <takashabe.public@gmail.com>
2017-07-07 00:36:07 +0900

    Implement server

--- forward/server.go
+++ forward/server.go
@@ -1,1 +1,88 @@
 package forward
+
+import (
+	"io"
+	"net"
+	"sort"
+	"time"
+
+	"github.com/takashabe/go-metrics/collect"
+)
+
+// NetWriter is forward to udp server
+type NetWriter struct {
+	Source      collect.Collector
+	Destination io.Writer
+	MetricsKeys []string
+	Interval    time.Duration
+}
+
+// NewNetWriter return new NetWriter
+func NewNetWriter(c collect.Collector, addr string) (MetricsWriter, error) {
+	if c == nil {
+		return nil, ErrInvalidCollector
+	}
+
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	w := &NetWriter{
+		MetricsKeys: make([]string, 0),
+		Interval:    time.Second,
+	}
+	w.SetSource(c)
+	w.SetDestination(conn)
+	return w, nil
+}
+
+func (cw *NetWriter) SetSource(c collect.Collector) {
+	cw.Source = c
+}
+
+func (cw *NetWriter) SetDestination(w io.Writer) {
+	cw.Destination = w
+}
+
+func (cw *NetWriter) AddMetrics(metrics ...string) error {
+	exists := cw.Source.GetMetricsKeys()
+	existMap := make(map[string]struct{})
+	for _, v := range exists {
+		existMap[v] = struct{}{}
+	}
+
+	for _, key := range metrics {
+		if _, ok := existMap[key]; !ok {
+			return ErrNotExistMetrics
+		}
+	}
+
+	cw.MetricsKeys = append(cw.MetricsKeys, metrics...)
+	sort.Strings(cw.MetricsKeys)
+	return nil
+}
+
+func (cw *NetWriter) RemoveMetrics(metrics ...string) error {
+	for _, m := range metrics {
+		for k, v := range cw.MetricsKeys {
+			if m == v {
+				cw.MetricsKeys = append(cw.MetricsKeys[:k], cw.MetricsKeys[k+1:]...)
+			}
+		}
+	}
+	return nil
+}
+
+func (cw *NetWriter) Flush() error {
+	buf, err := getMergedMetrics(cw.Source, cw.MetricsKeys...)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(cw.Destination, buf)
+	return err
+}
